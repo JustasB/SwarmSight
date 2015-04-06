5fe576dfbda6e56ed6da437947bf6cedf470ade1
birgiolasj <birgiolasj@yahoo.com>
2015-04-06 10:01:25 -0700

    SwarmVision Alpha 1
    Initial commit

---
+++ VideoPlayer/ComparedFrame.cs
@@ -0,0 +1,10 @@
+﻿using Justas.Bees.VideoPlayer;
+
+namespace Classes
+{
+    public class ComparedFrame
+    {
+        public Frame Frame;
+        public FrameComparerResults ComparerResults;
+    }
+}
